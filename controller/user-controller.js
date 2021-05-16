759c1332b8052e5fba1426a8999fb4410b26cccd
mememelm <carol.arivony@gmail.com>
2021-05-16 17:43:46 +0300

    git ignore

--- controller/user-controller.js
+++ controller/user-controller.js
@@ -1,3 +1,5 @@
+const db = require('../models/')
+
 const userTest = (req, res) => {
     try {
         res.status(200).json({ data: 'data REST', message: 'success' })
@@ -6,4 +8,18 @@ const userTest = (req, res) => {
     }
 }
 
-module.exports = { userTest }
+const create = async (req, res) => {
+    try {
+        const user = await db.user.build(req.body)
+        if (user) {
+            
+        }
+    } catch (error) {
+        res.status(400).json({ message: error })
+    }
+}
+
+module.exports = { 
+    userTest, 
+    create 
+}
